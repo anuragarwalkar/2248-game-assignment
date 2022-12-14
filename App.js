b0415324cbff45b1cc85eeee674b5c7ae600363e
Anurag Arwalkar <anurag.arwalkar@globant.com>
2022-12-14 23:16:40 +0530

    added basic grid

--- App.js
+++ App.js
@@ -1,33 +1,52 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 import gridGenerator from "./src/gridGenerator";
+
+const width = Dimensions.get("window").width;
 
 export default function App() {
   const [grid] = useState(gridGenerator.generate());
 
   return (
-    <View style={styles.container}>
-      {grid.map((i) => (
-        i.map(j => <View key={j.key} style={{ ...styles.box, backgroundColor: j.color }}>
-          <Text>{j.value}</Text>
-        </View>)
-      ))}
-    </View>
+      <View style={styles.container}>
+        <View style={styles.innerContainer}>
+          {grid.map((i) =>
+            i.map((j) => (
+              <View
+                key={j.key}
+                style={{ ...styles.box, backgroundColor: j.color }}
+              >
+                <Text style={{ color: "white", fontSize: 50 }}>{j.value}</Text>
+              </View>
+            ))
+          )}
+        </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 4,
+    backgroundColor: '#FFE7C3',
+    height: '100%',
     display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: "#fff",
+    justifyContent: 'center',
+  },
+  innerContainer: {
+    flexWrap: "wrap",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
     alignItems: "center",
     justifyContent: "center",
   },
   box: {
-    width: 50,
-    height: 50,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: width / 5,
+    height: width / 5,
+    borderRadius: 30,
+    margin: width * 0.02,
   },
 });
