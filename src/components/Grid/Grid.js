0f29560aaaa07da8d7a18aabf4e8ae696ca04a2d
Anurag Arwalkar <anurag.arwalkar@globant.com>
2022-12-22 11:56:42 +0530

    refactored

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,19 +1,12 @@
 import { FlatList, View } from "react-native";
 import useGridGenerator from "../../hooks/useGridGenerator";
 import { gridStyles } from "./styles";
-import GridItem from '../GridItem/GridItem';
 
 function Grid() {
-  const [grid] = useGridGenerator();
 
   return (
     <View style={gridStyles.innerContainer}>
-      <FlatList
-        data={grid}
-        numColumns={4}
-        scrollEnabled={false}
-        renderItem={({ item }) => <GridItem {...item} />}
-      />
+      
     </View>
   );
 }
--- src/components/GridItem/GridItem.js
+++ src/components/GridItem/GridItem.js
@@ -1,21 +1,7 @@
-import { StyleSheet, View, Text, TouchableWithoutFeedback } from "react-native";
-import { gridStyles } from "../Grid/styles";
+import { View } from "react-native";
 
 function GridItem({ id, value, color: backgroundColor, index }) {
-  const boxStyle = { ...gridStyles.box, backgroundColor, position: "relative" };
-
-  return (
-    <TouchableWithoutFeedback onPress={() => {
-      console.log(index);
-    }}>
-      <View key={id} style={boxStyle}>
-        <Text style={styles.text}>{value}</Text>
-      </View>
-      
-    </TouchableWithoutFeedback>
-  );
+  return <View></View>;
 }
-
-const styles = StyleSheet.create({ text: { color: "white", fontSize: 40 } });
 
 export default GridItem;
